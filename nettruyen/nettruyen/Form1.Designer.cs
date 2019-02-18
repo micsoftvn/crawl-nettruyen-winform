44f300dcaba6de50f876ba8d6c6f32d5bf386303
unknown <clonebmn2itt@gmail.com>
2019-02-18 17:27:45 +0700

    First commit

---
+++ nettruyen/nettruyen/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace nettruyen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLink = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtImages = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtLink
+            // 
+            this.txtLink.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLink.Location = new System.Drawing.Point(12, 42);
+            this.txtLink.Name = "txtLink";
+            this.txtLink.Size = new System.Drawing.Size(358, 25);
+            this.txtLink.TabIndex = 0;
+            this.txtLink.Text = "http://www.nettruyen.com/truyen-tranh/ashigei-shoujo-komura-san/chap-3/440095";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nhập link truyện cần leech";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(393, 44);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Leech";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtImages
+            // 
+            this.txtImages.Location = new System.Drawing.Point(12, 92);
+            this.txtImages.Multiline = true;
+            this.txtImages.Name = "txtImages";
+            this.txtImages.Size = new System.Drawing.Size(456, 158);
+            this.txtImages.TabIndex = 3;
+            this.txtImages.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 262);
+            this.Controls.Add(this.txtImages);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLink);
+            this.Name = "Form1";
+            this.Text = "Leach Truyện tại nettruyen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLink;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtImages;
+
+    }
+}
+
---
+++ nettruyen/nettruyen/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+namespace nettruyen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+           // progressBar1.Value = 0;
+           // progressBar1.Step = 1;
+
+           // for (int j = 0; j < 100; j++)
+           // {
+            //    System.Threading.Thread.Sleep(100);
+           //     progressBar1.PerformStep();
+            //}
+        }
+
+        private async void btnSubmit_Click(object sender, EventArgs e)
+        {
+            String textLink = txtLink.Text;
+            String textImages;
+
+            Regex regex = new Regex(@"^http://www.nettruyen.com/truyen-tranh/", RegexOptions.ECMAScript);
+
+            if (!regex.IsMatch(textLink))
+            {
+                MessageBox.Show("Link này không được hỗ trợ");
+                return;
+            }
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(textLink, HttpCompletionOption.ResponseContentRead);
+            String html = await response.Content.ReadAsStringAsync();
+            var parser = new HtmlWeb();
+            txtImages.Visible = true;
+        }
+
+    }
+}
